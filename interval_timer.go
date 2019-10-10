fadc4dea1cb6241a264be9641d1d56555704a181
Tom Myers <tom.myers@Zolv.com>
2019-10-10 16:31:29 +0100

    finished (somewhat) validation

--- interval_timer.go
+++ interval_timer.go
@@ -10,6 +10,17 @@ import (
 var sets int
 var work int
 var rest int
+var getSetsCounter = 0
+var getWorkCounter = 0
+var getRestCounter = 0
+
+func validatorExit() {
+	fmt.Println()
+	fmt.Println("you have tried three times now without getting it right...exiting")
+	fmt.Println()
+	main()
+
+}
 
 func working(w int) {
 
@@ -29,29 +40,43 @@ func resting(r int) {
 }
 
 func getSets() {
+	getSetsCounter++
+	if getSetsCounter > 3 {
+		getSetsCounter = 0
+		validatorExit()
+	}
 	fmt.Println("input the number of sets:")
 	fmt.Scan(&sets)
 	validate(sets, "sets")
-
 }
+
 func getWork() {
+	getWorkCounter++
+	if getWorkCounter > 3 {
+		getWorkCounter = 0
+		validatorExit()
+	}
 	fmt.Println("input the number of seconds per set:")
 	fmt.Scan(&work)
 	validate(work, "work")
-
 }
+
 func getRest() {
+	getRestCounter++
+	if getRestCounter > 3 {
+		getRestCounter = 0
+		validatorExit()
+	}
 	fmt.Println("input the number of seconds rest:")
 	fmt.Scan(&rest)
 	validate(rest, "rest")
-
 }
 
 func validate(x int, y string) bool {
-	fmt.Println("validating", y)
+	fmt.Println("validating", y, "value passed:", x)
 
-	if x < 1 && x > 600 {
-		fmt.Println(y, "must be greater than 0 and less than 600 (you are doing it wrong!!!)")
+	if x < 1 || x > 600 {
+		fmt.Println(y, "please enter a NUMBER greater than 0 and less than 600 (you are doing it wrong!!!)")
 		switch y {
 		case "rest":
 			getRest()
@@ -80,7 +105,7 @@ func validate(x int, y string) bool {
 }
 
 func main() {
-
+	fmt.Println()
 	fmt.Println("### Interval Timer ###")
 	// get sets, working time and resting time
 	// check that they are are int and not somethig ridiculous
@@ -89,10 +114,9 @@ func main() {
 	getRest()
 
 	fmt.Println("number of sets:", sets)
-
-	return
 	fmt.Println("working:", work)
 	fmt.Println("rest:", rest)
+	fmt.Println()
 	fmt.Println("starting in 5")
 	time.Sleep(1 * time.Second)
 	fmt.Println("starting in 4")
@@ -103,12 +127,18 @@ func main() {
 	time.Sleep(1 * time.Second)
 	fmt.Println("starting in 1")
 	time.Sleep(1 * time.Second)
+	fmt.Println()
 
 	for s := 1; s < sets+1; s++ {
 		fmt.Println("starting set :", s)
+		fmt.Println()
 		working(work)
+		fmt.Println()
+		resting(rest)
+		fmt.Println()
 
 	}
+	fmt.Println("*** Done! ***")
 	fmt.Println()
 
 }
