3be798031519366303bb4b6c167ecd51120ea473
Tom Myers <tom.myers@Zolv.com>
2019-11-15 15:53:14 +0000

    added create componant

--- AngularProject/src/app/create/create.component.ts
+++ AngularProject/src/app/create/create.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+    selector: 'app-create',
+    templateUrl: './create.component.html',
+    styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+    public timer: any;
 
-  ngOnInit() {
-  }
+    public constructor(private location: Location, private http: Http) {
+        this.timer = {
+            "name": "",
+            "data": {
+                "sets": false,
+                "work": false,
+                "rest": false
+            }
+        }
+    }
+
+    public ngOnInit() { }
+
+    public save() {
+        if(this.timer.name) {
+            this.http.post("http://localhost:12345/timer", JSON.stringify(this.timer))
+                .subscribe(result => {
+                    this.location.back();
+                });
+        }
+    }
 
 }
